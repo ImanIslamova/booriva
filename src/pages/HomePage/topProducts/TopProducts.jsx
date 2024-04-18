932f713d7a0dcdb6034eae2479771fd10f9aa9e7
zarineo <tochilka007@mail.ru>
2024-04-18 13:27:46 +0300

    категории

---
+++ src/pages/HomePage/topProducts/TopProducts.jsx
@@ -0,0 +1,10 @@
+import styles from './topProducts.module.sass';
+
+const TopProducts = () => {
+    return (
+        <div className = {`${styles.topProducts} wrapper`}> 
+            
+        
+        </div>
+    )
+}

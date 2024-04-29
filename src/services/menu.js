7d148eeea9c981687ff20f05b55ca597f2308dac
Iman Islamova <fff_1992@internet.ru>
2024-04-29 17:53:24 +0300

    feat: subMenu реализация запроса

--- src/services/menu.js
+++ src/services/menu.js
@@ -3,3 +3,9 @@ export const getMenuItems = async () =>{
     res = await res.json()
     return res
 }
+
+export const getSubMenuItems = async (id) =>{
+    let res = await fetch(`https://640ef1d54ed25579dc40e2a6.mockapi.io/categories?menuId=${id}`)
+    res = await res.json()
+    return res[0].categories
+}

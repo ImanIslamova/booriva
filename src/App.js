e8ef2626b72313ee618c51a67f85cfa295318cc3
Iman Islamova <fff_1992@internet.ru>
2024-05-10 11:04:50 +0300

    верстка корзины

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import WishList from "./pages/wishList/WishList";
 import Catalog from "./pages/Catalog/Catalog";
 import Nav from "./components/layot/nav/Nav";
 import ProductPage from './pages/ProductPage/ProductPage';
+import Basket from './components/layot/basket/basket';
 
 function App() {
   return (
     <div>
+      <Basket />
       <Nav />
       <Routes>
         <Route path='/' element={<HomePage />}/>
---
+++ src/components/layot/basket/basket.jsx
@@ -0,0 +1,9 @@
+import styles from './basket.module.sass'
+
+const Basket = () =>{
+    return <div className={styles.basket__wrap}>
+        <div className={styles.basket}></div>
+    </div>
+}
+
+export default Basket
